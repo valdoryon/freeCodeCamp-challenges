6040b1eee1483cad0a45172e9056392f8e4d43bc
valdoryon <valdoryon@gmail.com>
2023-10-23 14:54:44 -0300

    first challenge ready

--- Random-Quote-Gen/src/App.jsx
+++ Random-Quote-Gen/src/App.jsx
@@ -4,18 +4,18 @@ import jsonData from './quotes-list.json'
 import { useState } from 'react'
 function App () {
   const newIndex = () => {
-    setIndex(Math.floor(Math.random() * 102))
+    return Math.floor(Math.random() * 102)
   }
 
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(newIndex)
 
   return (
     <>
-      <div className='app-wrapper'>
+      <div className='app-wrapper' style={{ backgroundColor: 'var(--main-background-color)' }}>
         <Quotes
           quote={jsonData.quotes[index].quote}
           author={jsonData.quotes[index].author}
-          handleClick={newIndex}
+          handleClick={() => setIndex(newIndex)}
         />
       </div>
 
--- Random-Quote-Gen/src/Components/Quotes/Quotes.jsx
+++ Random-Quote-Gen/src/Components/Quotes/Quotes.jsx
@@ -11,9 +11,10 @@ const Quotes = (props) => {
         <span id='author'>-{props.author}</span>
       </div>
       <div id='buttons'>
-        <div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
           <a id='tweet-quote' href={`https://www.twitter.com/intent/tweet?hashtags=quotes&text=${props.quote}`}><img id='icon' src='/twitter-icon.svg' /></a>
-          <a id='github' href='https://www.github.com'><img id='icon' src='/github-icon.svg' /></a>
+          <div style={{ width: 25 }} />
+          <a id='github' href='https://github.com/valdoryon/freeCodeCamp-challenges/tree/main/Random-Quote-Gen'><img id='icon' src='/github-icon.svg' /></a>
         </div>
         <button onClick={props.handleClick} id='new-quote'>New quote</button>
 
